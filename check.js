63f02e652359e437908043bcf59cf3ca8e2faec1
kelabang <imam.tauhid.dar@gmail.com>
2016-05-12 18:55:09 +0700

    up

--- check.js
+++ check.js
@@ -25,7 +25,7 @@ function getIfeedCurried (symbol) {
 	}
 }
 console.log(' -->> start request ')
-request('http://127.0.0.1/stockbit/api/company', function (error, response, body) {
+request('http://127.0.0.1/company', function (error, response, body) {
 	if (!error && response.statusCode == 200) {
 		var body = JSON.parse(body)
 		var que = []
